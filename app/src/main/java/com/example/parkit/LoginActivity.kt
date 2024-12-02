9afae30b1488356e82db2fdfc53d1e34ea4e3165
sofia <sofiagandra2018@gmail.com>
2024-12-02 18:10:47 +0000

    pagina login

--- app/src/main/java/com/example/parkit/LoginActivity.kt
+++ app/src/main/java/com/example/parkit/LoginActivity.kt
@@ -11,18 +11,15 @@ import com.google.firebase.auth.FirebaseAuth
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
-    // Declaração dos campos de input
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
-    private lateinit var registerButton: Button // Para redirecionar para a tela de registro
+    private lateinit var registerButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // Inicializando o FirebaseAuth
         auth = FirebaseAuth.getInstance()
 
         // Associando as views com os elementos do layout
@@ -42,25 +39,20 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show()
             }
         }
-
-        // Evento de clique para redirecionar para a tela de registro (caso o usuário não tenha conta)
         registerButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
     }
 
-    // Função para autenticar o usuário
     private fun loginUser(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Se a autenticação for bem-sucedida, redireciona para a MainActivity
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
-                    finish() // Fecha a LoginActivity
-                } else {
-                    // Se a autenticação falhar, mostra uma mensagem de erro
+                    finish()
+                } else
                     Toast.makeText(this, "Falha na autenticação: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                 }
             }
