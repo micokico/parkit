a5b778e1f72fb06db77582b091b802f874e7c3dd
sofia <sofiagandra2018@gmail.com>
2024-12-02 16:09:50 +0000

    pagina 3

---
+++ app/src/main/java/com/example/parkit/NavigationActivity.kt
@@ -0,0 +1,25 @@
+package com.example.parkit
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+
+        val skipButton: TextView = findViewById(R.id.skip_button)
+        val nextButton: TextView = findViewById(R.id.next_button)
+
+        skipButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java)) // Redirecione para a próxima tela do app
+            finish()
+        }
+
+        nextButton.setOnClickListener {
+            startActivity(Intent(this, NextOnboardingActivity::class.java))
+        }
+    }
+}
