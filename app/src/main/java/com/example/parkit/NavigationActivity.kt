5c7d91102284dcea3b9dcd14d8d4f46b016ed51b
sofia <sofiagandra2018@gmail.com>
2024-12-04 22:46:57 +0000

    correções

--- app/src/main/java/com/example/parkit/NavigationActivity.kt
+++ app/src/main/java/com/example/parkit/NavigationActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.onboarding.PaymentActivity
+
 
 class NavigationActivity : AppCompatActivity() {
 
