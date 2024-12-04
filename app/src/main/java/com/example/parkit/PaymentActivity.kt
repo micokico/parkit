b29133c808b5ba1c8e6c2de8ca8444d22e403a68
sofia <sofiagandra2018@gmail.com>
2024-12-04 23:17:28 +0000

    correções

--- app/src/main/java/com/example/parkit/PaymentActivity.kt
+++ app/src/main/java/com/example/parkit/PaymentActivity.kt
@@ -2,7 +2,9 @@ package com.example.parkit
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.parkit.LoginActivity
 import com.example.parkit.R
@@ -14,8 +16,14 @@ class PaymentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_onboarding_screen3)
         val startButton: Button = findViewById(R.id.Start)
         startButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            try {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent) // Navega para LoginActivity
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "Erro ao navegar: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 }
