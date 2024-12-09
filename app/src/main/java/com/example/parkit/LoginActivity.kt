81e6680690af84ad88357f3699dd963347d3861c
sofia <sofiagandra2018@gmail.com>
2024-12-09 23:05:41 +0000

    correções

--- app/src/main/java/com/example/parkit/LoginActivity.kt
+++ app/src/main/java/com/example/parkit/LoginActivity.kt
@@ -2,28 +2,27 @@ package com.example.parkit
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.parkit.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inicialização do view binding
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Inicialização do Firestore
+        auth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
 
-        // Ação do botão "Login"
         binding.loginButton.setOnClickListener {
             val phoneNumber = binding.NumeroEditText.text.toString().trim()
             val password = binding.passwordEditText.text.toString().trim()
@@ -36,21 +35,16 @@ class LoginActivity : AppCompatActivity() {
             loginUser(phoneNumber, password)
         }
 
-        // Ação do botão "Criar Conta"
         binding.registerButton.setOnClickListener {
             startActivity(Intent(this, PhoneNumberActivity::class.java))
         }
 
-        // Ação do texto "Esqueceu-se da palavra-passe?"
-        val tvForgotPassword = findViewById<TextView>(R.id.tvForgotPassword)
-        tvForgotPassword.setOnClickListener {
-            val intent = Intent(this, ForgotPasswordActivity::class.java)
-            startActivity(intent)
+        binding.forgotPasswordTextView.setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
         }
     }
 
     private fun loginUser(phoneNumber: String, password: String) {
-        // Buscar as credenciais no Firestore
         db.collection("users").document(phoneNumber)
             .get()
             .addOnSuccessListener { document ->
@@ -72,7 +66,6 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun goToHomeScreen() {
-        // Navega para a HomeActivity e limpa a pilha de atividades anteriores
         val intent = Intent(this, HomeActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
