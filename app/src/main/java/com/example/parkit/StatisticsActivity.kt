65deaef653dc2ce9bdc64f052642f19b8cac47f8
sofia <sofiagandra2018@gmail.com>
2024-12-26 17:38:29 +0000

    correções

--- app/src/main/java/com/example/parkit/StatisticsActivity.kt
+++ app/src/main/java/com/example/parkit/StatisticsActivity.kt
@@ -1,7 +1,8 @@
 package com.example.parkit
 
+import android.content.Intent  // Importação da classe Intent
 import android.os.Bundle
-import android.widget.Button  // Importação da classe Button
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.parkit.R
 
@@ -14,18 +15,21 @@ class StatisticsActivity : AppCompatActivity() {
         // Configura o botão de voltar
         val btnBack = findViewById<Button>(R.id.btnBack)
         btnBack.setOnClickListener {
-            finish()
+            // Cria uma Intent para voltar à tela AdminDashboardActivity
+            val intent = Intent(this, AdminDashboardActivity::class.java)
+            startActivity(intent)  // Inicia a AdminDashboardActivity
+            finish()  // Fecha a StatisticsActivity
         }
 
         // Configura botões
         val btnTotalUsers = findViewById<Button>(R.id.buttonUserCount)
         btnTotalUsers.setOnClickListener {
-
+            // Ação para exibir quantidade de utilizadores
         }
 
         val btnGlobalRevenue = findViewById<Button>(R.id.buttonGlobalRevenue)
         btnGlobalRevenue.setOnClickListener {
-
+            // Ação para exibir receitas globais
         }
     }
 }
